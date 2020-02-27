8059cd29cfa5a92c51e31cc7a99b057bd10d4d11
zhehao.chen <zhehao.chen@ikang.com>
2020-02-27 17:01:48 +0800

    eureka-provider&consumer

--- springcloud-eureka/eureka-service-consumer/src/main/java/com/liziczh/eureka/service/consumer/controller/EurekaServiceConsumerController.java
+++ springcloud-eureka/eureka-service-consumer/src/main/java/com/liziczh/eureka/service/consumer/controller/EurekaServiceConsumerController.java
@@ -1,28 +1,27 @@
-package com.liziczh.nacos.discovery.consumer.controller;
+package com.liziczh.eureka.service.consumer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-/**
- * @author zhehao.chen
- */
 @RestController
-@RequestMapping("/consumer/")
-public class NacosDiscoveryConsumerController {
+public class EurekaServiceConsumerController {
 	@Bean
 	@LoadBalanced
-	public RestTemplate getRestTemplate() {
+	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
+
 	@Autowired
 	private RestTemplate restTemplate;
-	@GetMapping(value = "get")
-	public String consumer() {
-		return restTemplate.getForObject("http://nacos-provider/provider/out/123456", String.class);
+
+	@GetMapping(value = "/get/{value}")
+	public String get(@PathVariable String value){
+		String url = "http://eureka-service-provider:8001/out/"+value;
+		return restTemplate.getForObject(url, String.class);
 	}
 }
