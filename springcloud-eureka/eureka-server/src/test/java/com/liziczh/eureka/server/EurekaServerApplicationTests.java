ec1ad9ccfd04427fa8fc4f870ff92ce92485354e
liziczh <liziczh@foxmail.com>
2020-02-27 13:35:33 +0800

    eureka

---
+++ springcloud-eureka/eureka-server/src/test/java/com/liziczh/eureka/server/EurekaServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.liziczh.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
