e2bb7c4a97e802435f0ce7cb8645f53c232a5ce0
liziczh <liziczh@foxmail.com>
2020-02-27 18:37:50 +0800

    feign

--- springcloud-eureka/eureka-service-consumer/src/main/java/com/liziczh/eureka/service/consumer/EurekaServiceConsumerApplication.java
+++ springcloud-eureka/eureka-service-consumer/src/main/java/com/liziczh/eureka/service/consumer/EurekaServiceConsumerApplication.java
@@ -2,8 +2,12 @@ package com.liziczh.eureka.service.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class EurekaServiceConsumerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServiceConsumerApplication.class, args);
---
+++ springcloud-eureka/eureka-service-consumer/src/main/java/com/liziczh/eureka/service/consumer/remote/FeignService.java
@@ -0,0 +1,7 @@
+package com.liziczh.eureka.service.consumer.remote;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "", path = "")
+public interface FeignService {
+}
