803ad93421b1a11d9c33506dba9f7b23b7ef0544
liziczh <liziczh@foxmail.com>
2020-03-02 15:26:36 +0800

    config

---
+++ springcloud-config/config-client/src/test/java/com/liziczh/config/client/ConfigClientApplicationTests.java
@@ -0,0 +1,11 @@
+package com.liziczh.config.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
